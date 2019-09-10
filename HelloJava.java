10613d7d3ef2571e7ad61890aef4790893fe8b73
naveen kommi <naveenk@unifytech.com>
2019-09-10 15:37:06 +0530

    initial commit

---
+++ HelloJava.java
@@ -0,0 +1,12 @@
+class HiJava{
+void get(){
+System.out.println("hey hello java");
+}
+}
+public class HelloJava{
+public static void main(String[] args){
+
+HiJava hijava=new HiJava();
+hijava.get();
+}
+}
